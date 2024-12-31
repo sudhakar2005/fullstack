2a184e2ebd6f9e1296b1b7d45fa721cec5ed5be0
sudhakar2005 <spssudhakaran2005@gmail.com>
2024-12-31 08:51:46 +0530

    4th day project

---
+++ fullstack/src/Components/FunctionalComponent/NavBar.jsx
@@ -0,0 +1,17 @@
+
+import '../../assets/css/NavBar.css'
+var NavBar = () =>{
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li><h2>Home</h2></li>
+                    <li><h2>About</h2></li>
+                    <li><h2>Gallery</h2></li>
+                    <li><h2>Contact</h2></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default NavBar;
