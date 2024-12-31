0dceba6524165ba3f4b7e578c06ab4314e810f5e
sudhakar2005 <spssudhakaran2005@gmail.com>
2024-12-31 10:16:20 +0530

    webpage deployment

---
+++ fullstack/operator.js
@@ -0,0 +1,49 @@
+//assignment operator
+a=10
+console.log(a);
+
+//unary operator
+a++; //post increment
+console.log(a);
+
+//arthimetic operator
+x=10
+y=5
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+
+//relational operator
+
+c=26
+d="26"
+if(c==d)
+{
+    console.log("same number");
+}
+ 
+//logical operator
+
+log1=10
+log2=23
+console.log(log1 & log2);
+
+//bitwise operator
+
+bit1=10
+bit2=25
+bit3=20
+if(bit1<bit2 && bit1>bit3)
+{
+    console.log(bit1,"is smaller");
+}
+else if(bit2<bit1 && bit2>bit3)
+{
+    console.log(bit2,"is smaller");
+}
+else{
+    console.log(bit3,"is smaller");
+}
+
+
