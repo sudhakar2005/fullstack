e6c1d4fc1e12e5abc7fb27df0de3fda87a90bfe8
sudhakar2005 <spssudhakaran2005@gmail.com>
2025-01-07 09:52:54 +0530

    8th day

--- src/Components/FunctionalComponent/Home.jsx
+++ src/Components/FunctionalComponent/Home.jsx
@@ -2,15 +2,20 @@
 import '../../assets/css/Home.css'
 var Home = () =>{
     var styling = {
-        fontSize: "50px",
+        fontSize: "25px",
         color: "blue",
         textDecoration: "underline",
+        textAlign: "center",
+    }
+    var style = {
+        fontSize: "25px",
+        color: "red",
         textAlign: "center",
     }
     return(
         <header>
             <h1 style={styling}>This is Home Page</h1>
-            <h2 id='idSEg'>Testing Styling</h2>
+            <h2 id='idSEg' style={style}>Testing Styling</h2>
             <h1 className='box-model'>Hello KEC</h1>
         </header>
     )
